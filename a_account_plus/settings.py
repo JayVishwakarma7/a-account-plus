723e451f92c05c03471150ef40e84cb5db1fd4ab
ArmaVarma1 <155721639+ArmaVarma1@users.noreply.github.com>
2026-07-02 15:35:07 +0530

    Fixed email timeout and switched to SSL port 465

--- a_account_plus/settings.py
+++ a_account_plus/settings.py
@@ -86,12 +86,12 @@ LOGIN_URL = '/admin/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = "jayvishwakarmahere@gmail.com"        
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_TIMEOUT = 10  # 10 seconds timeout
-EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30  # 30 seconds timeout
+EMAIL_USE_SSL = True
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CONTACT_RECEIVER_EMAIL = "jayvishwakarmahere@gmail.com"  
